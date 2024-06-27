c64d06de429ee32e7b85557036313a7f09476712
mini angle <96669024+loy0516@users.noreply.github.com>
2024-06-28 01:50:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const WebSocket = require('ws');
 const logcb = (...args) => console.log.bind(this, ...args);
 const errcb = (...args) => console.error.bind(this, ...args);
 
-const uuid = (process.env.UUID || '37a0bd7c-8b9f-4693-8916-bd1e2da0a817').replace(/-/g, '');
-const port = process.env.PORT || 3000;
+const uuid = (process.env.UUID || '13468b84-3b3c-4bba-95cb-af84d3d7ea97').replace(/-/g, '');
+const port = process.env.PORT || 47395;
 
 const wss = new WebSocket.Server({ port }, logcb('listen:', port));
 
